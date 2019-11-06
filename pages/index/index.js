353f0a73abc21b11c06cbd536c6dcda1f08fa56b
peiboning <boningpei@sohu-inc.com>
2019-11-06 19:32:11 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,724 @@
+let utils = require('../../utils/util')
+let globalData = getApp().globalData
+const key = globalData.key
+let SYSTEMINFO = globalData.systeminfo
+var weekArray = new Array("日", "一", "二", "三", "四", "五", "六");
+Page({
+  data: {
+    isShowLocationTips:false,
+    showKnowledgeDot:1,
+    backgroundImage:'',
+    air:{}, 
+    searchAlpha:1.0,
+    searchHeight: 0,
+    isShowSearch: true,
+    isIPhoneX: globalData.isIPhoneX,
+    message: '',
+    cityDatas: {},
+    hourlyDatas: [],
+    weatherIconUrl: globalData.weatherIconUrl,
+    detailsDic: {
+      key: ['tmp', 'fl', 'hum', 'pcpn', 'wind_dir', 'wind_deg', 'wind_sc', 'wind_spd', 'vis', 'pres', 'cloud', ''],
+      val: {
+        tmp: '温度(℃)',
+        fl: '体感温度(℃)',
+        hum: '相对湿度(%)',
+        pcpn: '降水量(mm)',
+        wind_dir: '风向',
+        wind_deg: '风向角度(deg)',
+        wind_sc: '风力(级)',
+        wind_spd: '风速(mk/h)',
+        vis: '能见度(km)',
+        pres: '气压(mb)',
+        cloud: '云量',
+      },
+    },
+    lifestyles: {
+      'comf': '舒适度指数',
+      'cw': '洗车指数',
+      'drsg': '穿衣指数',
+      'flu': '感冒指数',
+      'sport': '运动指数',
+      'trav': '旅游指数',
+      'uv': '紫外线指数',
+      'air': '空气污染扩散条件指数',
+      'ac': '空调开启指数',
+      'ag': '过敏指数',
+      'gl': '太阳镜指数',
+      'mu': '化妆指数',
+      'airc': '晾晒指数',
+      'ptfc': '交通指数',
+      'fsh': '钓鱼指数',
+      'spi': '防晒指数',
+    },
+    // 用来清空 input
+    searchText: '',
+    // 是否已经弹出
+    hasPopped: false,
+    animationMain: {},
+    animationOne: {},
+    animationTwo: {},
+    animationThree: {},
+    animationHiddenGuidView:{},
+    // 是否切换了城市
+    located: true,
+    // 需要查询的城市
+    searchCity: '',
+    setting: {},
+    richu:'/img/richu.png',
+    riluo:'/img/rila.png',
+    bcgImgIndex: 0,
+    bcgImg: '',
+    bcgImgAreaShow: false,
+    bcgColor: '#2d2225',
+    // 粗暴直接：移除后再创建，达到初始化组件的作用
+    showHeartbeat: true,
+    // heartbeat 时禁止搜索，防止动画执行
+    enableSearch: true,
+    openSettingButtonShow: false,
+    shareInfo: {
+      "title":"生活天气"
+    },
+  },
+  success (data, location) {
+    this.setData({
+      openSettingButtonShow: false,
+      searchCity: location,
+    })
+    wx.stopPullDownRefresh()
+    //添加星期信息
+    var daily_forecast = data.daily_forecast;
+    for(var i =0;i<daily_forecast.length;i++){
+      var temp = daily_forecast[i];
+      var week = this.getWeek(temp.date);
+      temp.week = week;
+    }
+    
+    let now = new Date()
+    // 存下来源数据
+    data.updateTime = now.getTime()
+    data.updateTimeFormat = utils.formatDate(now, "MM-dd hh:mm")
+    wx.setStorage({
+      key: 'cityDatas',
+      data,
+    })
+    this.setData({
+      cityDatas: data,
+    })
+  },
+  fail(res) {
+    wx.stopPullDownRefresh()
+    let errMsg = res.errMsg || ''
+    // 拒绝授权地理位置权限
+    if (errMsg.indexOf('deny') !== -1 || errMsg.indexOf('denied') !== -1) {
+      this.setData({
+        isShowLocationTips:true
+      })
+      wx.showToast({
+        title: '需要开启地理位置权限',
+        icon: 'none',
+        duration: 2500,
+        success: (res) => {
+          // if (this.canUseOpenSettingApi()) {
+          //   // let timer = setTimeout(() => {
+          //   //   clearTimeout(timer)
+          //   //   wx.openSetting({
+          //   //     success(res) {
+          //   //       console.log("openSetting:"+res.authSetting)
+          //   //       // res.authSetting = {
+          //   //       //   "scope.userInfo": true,
+          //   //       //   "scope.userLocation": true
+          //   //       // }
+          //   //     }
+          //   //   })
+          //   // }, 2500)
+          // } else {
+          //   this.setData({
+          //     openSettingButtonShow: true,
+          //   })
+          // }
+        },
+      })
+    } else {
+      wx.showToast({
+        title: '网络不给力，请稍后再试',
+        icon: 'none',
+      })
+    }
+  },
+  commitSearch (res) {
+    let val = ((res.detail || {}).value || '').replace(/\s+/g, '')
+    this.search(val)
+  },
+  dance() {
+    this.setData({
+      enableSearch: false,
+    })
+    let heartbeat = this.selectComponent('#heartbeat')
+    heartbeat.dance(() => {
+      this.setData({
+        showHeartbeat: false,
+        enableSearch: true,
+      })
+      this.setData({
+        showHeartbeat: true,
+      })
+    })
+  },
+  clearInput () {
+    this.setData({
+      searchText: '',
+    })
+  },
+  search (val, callback) {
+    if (val === '520' || val === '521') {
+      this.clearInput()
+      this.dance()
+      return
+    }
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration: 300,
+    })
+    if (val) {
+      this.setData({
+        located: false,
+      })
+      this.getWeather(val)
+      this.getHourly(val)
+    }
+    callback && callback()
+  },
+  // wx.openSetting 要废弃，button open-type openSetting 2.0.7 后支持
+  // 使用 wx.canIUse('openSetting') 都会返回 true，这里判断版本号区分
+  canUseOpenSettingApi () {
+    let systeminfo = getApp().globalData.systeminfo
+    let SDKVersion = systeminfo.SDKVersion
+    let version = utils.cmpVersion(SDKVersion, '2.0.7')
+    if (version < 0) {
+      return true
+    } else {
+      return false
+    }
+  },
+  init(params, callback) {
+    this.setData({
+      located: true,
+    })
+    wx.getLocation({
+      success: (res) => {
+        console.log(res)
+        this.getWeather(`${res.latitude},${res.longitude}`)
+        this.getHourly(`${res.latitude},${res.longitude}`)
+        callback && callback()
+        
+
+      },
+      fail: (res) => {
+        this.fail(res)
+      }
+    })
+  },
+
+  getAirInfo(data){
+    var that = this;
+    var location = data.basic.parent_city;
+    wx.request({
+      url: `${globalData.requestUrl.air}`,
+      data: {
+        location,
+        key,
+      },
+      success: (res) => {
+        if (res.statusCode === 200) {
+          let data = res.data.HeWeather6[0]
+          if (data.status === 'ok') {
+            that.setData({
+              air: data.air_now_city}
+            )
+          }
+        }
+      },
+      fail: () => {
+      },
+    })
+  },
+
+  getWeather (location) {
+    wx.request({
+      url: `${globalData.requestUrl.weather}`,
+      data: {
+        location,
+        key,
+      },
+      success: (res) => {
+        if (res.statusCode === 200) {
+          let data = res.data.HeWeather6[0]
+          if (data.status === 'ok') {
+            this.clearInput()
+            this.success(data, location)
+            this.getAirInfo(data)
+          } else {
+            wx.showToast({
+              title: '查询失败',
+              icon: 'none',
+            })
+          }
+        }
+      },
+      fail: () => {
+        wx.showToast({
+          title: '查询失败',
+          icon: 'none',
+        })
+      },
+    })
+  },
+  getHourly(location) {
+    wx.request({
+      url: `${globalData.requestUrl.hourly}`,
+      data: {
+        location,
+        key,
+      },
+      success: (res) => {
+        if (res.statusCode === 200) {
+          let data = res.data.HeWeather6[0]
+          if (data.status === 'ok') {
+            this.setData({
+              hourlyDatas: data.hourly || []
+            })
+          }
+        }
+      },
+      fail: () => {
+        wx.showToast({
+          title: '查询失败',
+          icon: 'none',
+        })
+      },
+    })
+  },
+  onPullDownRefresh (res) {
+    this.setBcgImg1();
+    this.reloadPage();
+  },
+  getCityDatas() {
+    let cityDatas = wx.getStorage({
+      key: 'cityDatas',
+      success: (res) => {
+        this.setData({
+          cityDatas: res.data,
+        })
+      },
+    })
+  },
+  setBcgImg1(){
+    var data = new Date();
+    var hours = data.getHours();
+    var date = data.getDate();
+    var month = data.getMonth() + 1;
+    var color;
+    console.log('now hours is :' + hours);
+    var path = "";
+    if(hours >= 20 || hours<=5){
+      path = '/img/yewan.jpg';
+      color = '#061824';
+    }else if(hours > 17){
+      path = '/img/bangwan.jpg';
+      color = '#383b3d';
+    }else if(hours<7){
+      path = '/img/zaochen.jpg';
+      color = '#6c93bb';
+    }else{
+      color = '#3a96f5';
+      path = '/img/baitian.jpg';
+    }
+
+    //国庆节背景
+    console.log('month :' + month + ',date:' + date);
+    if ((month == 9 && date > 1) || month == 10) {
+      color = '#a72922';
+      path = '/img/guoqing.png';
+    }
+    //中秋节背景
+    if ((month == 9 && date > 10 && date<16)) {
+      color = '#364362';
+      path = '/img/zhongqiu.png';
+    }
+    console.log('path :' + path);
+    this.setData({
+      bcgImg: path,
+      bcgColor: color,
+    })
+    this.setNavigationBarColor();
+    
+  },
+   
+  setNavigationBarColor (color) {
+    let bcgColor = color || this.data.bcgColor
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: this.data.bcgColor,
+    })
+  },
+  getBroadcast (callback) {
+    wx.cloud.callFunction({
+      name: 'getBroadcast',
+      data: {
+        hour: new Date().getHours(),
+      },
+    })
+    .then(res => {
+      let data = res.result.data
+      if (data) {
+        callback && callback(data[0].message)
+      }
+    })
+  },
+  reloadGetBroadcast () {
+    this.getBroadcast((message) => {
+      this.setData({
+        message,
+      })
+    })
+  },
+  reloadWeather () {
+    if (this.data.located) {
+      this.init({})
+    } else {
+      this.search(this.data.searchCity)
+      this.setData({
+        searchCity: '',
+      })
+    }
+  },
+  onShow() {
+    console.log("onShow..............");
+    wx.getSystemInfo({
+      success: function(res) {
+        console.log("getSystemInfo.............." + res.SDKVersion);
+      },
+    })
+    this.reloadKnowledgeDotStatus();
+    var that = this;
+    // 检查权限
+    wx.getSetting({
+      success: function (res){
+        if (res.authSetting["scope.userLocation"] == true){
+          if(that.data.isShowLocationTips){
+            that.setData({
+              isShowLocationTips: false
+            })
+            that.reloadPage()
+          }
+          
+        }else{
+          
+          that.setData({
+            isShowLocationTips: true
+          })
+          wx.showToast({
+            title: '获取地理位置权限失败',
+            icon: 'none',
+          })
+        }
+      }
+    })
+
+    setTimeout(function(){
+      that.hiddenGuidView();
+    }, 6000)
+  },
+  onLoad () {
+    var that = this;
+    this.setBcgImg1();//按时间来定背景
+    this.reloadPage();
+    console.log('onloaded');
+    var query = wx.createSelectorQuery();
+    query.select("#search").boundingClientRect();
+    query.exec(function (res) {
+      that.data.searchHeight = res[0].height;
+      console.log("search height is :" + that.data.searchHeight);
+    })
+
+
+  },
+  reloadPage () {
+    
+    this.getCityDatas()
+    this.reloadInitSetting()
+    this.reloadWeather()
+    
+  },
+  checkUpdate (setting) {
+    // 兼容低版本
+    if (!setting.forceUpdate || !wx.getUpdateManager) {
+      return
+    }
+    let updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate((res) => {
+      console.error(res)
+    })
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已下载完成，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+  },
+  showBcgImgArea () {
+    this.setData({
+      bcgImgAreaShow: true,
+    })
+  },
+  hideBcgImgArea () {
+    this.setData({
+      bcgImgAreaShow: false,
+    })
+  },
+  chooseBcg (e) {
+    let dataset = e.currentTarget.dataset
+    let src = dataset.src
+    let index = dataset.index
+    wx.setStorage({
+      key: 'bcgImgIndex',
+      data: index,
+    })
+  },
+  toCitychoose () {
+    wx.navigateTo({
+      url: '/pages/citychoose/citychoose',
+    })
+  },
+  initSetting (successFunc) {
+    wx.getStorage({
+      key: 'setting',
+      success: (res) => {
+        let setting = res.data || {}
+        this.setData({
+          setting,
+        })
+        successFunc && successFunc(setting)
+      },
+      fail: () => {
+        this.setData({
+          setting: {},
+        })
+      },
+    })
+  },
+  reloadInitSetting () {
+    this.initSetting((setting) => {
+      this.checkUpdate(setting)
+    })
+  },
+  onShareAppMessage (res) {
+
+    var data = new Date();
+    var hours = data.getHours();
+    var date = data.getDate();
+    var month = data.getMonth() + 1;
+    let shareInfo = this.data.shareInfo
+    var shareTitle = '关注生活, 关注天气'
+
+    //国庆节分享语
+    console.log('month :' + month + ',date:' + date);
+    if ((month == 9 && date > 20)) {
+      shareTitle = '国庆佳节,关注出行天气';
+    }
+    //中秋节分享语
+    if ((month == 9 && date > 10 && date < 16)) {
+      shareTitle = '中秋佳节,关注出行天气';
+    }
+
+    return {
+      title: shareTitle,
+      path: shareInfo.path || '/pages/index/index',
+      imageUrl: shareInfo.imageUrl,
+    }
+  },
+  menuHide () {
+    if (this.data.hasPopped) {
+      this.takeback()
+      this.setData({
+        hasPopped: false,
+      })
+    }
+  },
+  menuMain () {
+    if (!this.data.hasPopped) {
+      this.popp()
+      this.setData({
+        hasPopped: true,
+      })
+    } else {
+      this.takeback()
+      this.setData({
+        hasPopped: false,
+      })
+    }
+  },
+  menuToCitychoose () {
+    this.menuMain()
+    wx.navigateTo({
+      url: '/pages/citychoose/citychoose',
+    })
+  },
+  menuToSetting () {
+    this.menuMain()
+    wx.navigateTo({
+      url: '/pages/setting/setting',
+    })
+  },
+  menuToAbout () {
+    this.menuMain()
+    wx.navigateTo({
+      url: '/pages/about/about',
+    })
+  },
+  popp() {
+    let animationMain = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease-out'
+    })
+    let animationOne = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease-out'
+    })
+    let animationTwo = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease-out'
+    })
+    let animationThree = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease-out'
+    })
+    let animationFour = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease-out'
+    })
+    animationMain.rotateZ(180).step()
+    animationOne.translate(0, -60).rotateZ(360).opacity(1).step()
+    animationTwo.translate(-Math.sqrt(3600 - 400), -30).rotateZ(360).opacity(1).step()
+    animationThree.translate(-Math.sqrt(3600 - 400), 30).rotateZ(360).opacity(1).step()
+    animationFour.translate(0, 60).rotateZ(360).opacity(1).step()
+    this.setData({
+      animationMain: animationMain.export(),
+      animationOne: animationOne.export(),
+      animationTwo: animationTwo.export(),
+      animationThree: animationThree.export(),
+      animationFour: animationFour.export(),
+    })
+  },
+  takeback() {
+    let animationMain = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease-out'
+    })
+    let animationOne = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease-out'
+    })
+    let animationTwo = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease-out'
+    })
+    let animationThree = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease-out'
+    })
+    let animationFour = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease-out'
+    })
+    animationMain.rotateZ(0).step();
+    animationOne.translate(0, 0).rotateZ(0).opacity(0).step()
+    animationTwo.translate(0, 0).rotateZ(0).opacity(0).step()
+    animationThree.translate(0, 0).rotateZ(0).opacity(0).step()
+    animationFour.translate(0, 0).rotateZ(0).opacity(0).step()
+    this.setData({
+      animationMain: animationMain.export(),
+      animationOne: animationOne.export(),
+      animationTwo: animationTwo.export(),
+      animationThree: animationThree.export(),
+      animationFour: animationFour.export(),
+    })
+  },
+  hiddenGuidView() {
+    let animationHidden = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-out',
+    })
+    animationHidden.opacity(0).step()
+    this.setData({
+      animationHiddenGuidView: animationHidden.export()
+    })
+  },
+
+  onPageScroll: function (ev) {
+    console.log("scroll top is " + ev.scrollTop)
+    var scrollTop = ev.scrollTop;
+    if (scrollTop > this.data.searchHeight){
+      this.setData({
+        searchAlpha:0
+      })
+    }else{
+      var alph = (this.data.searchHeight - scrollTop) / this.data.searchHeight;
+      console.log("scroll alph " + alph)
+      this.setData({
+        searchAlpha: alph
+      })
+    }
+
+  },
+
+  getWeek(date){
+    var week = new Date(date).getDay();
+    return weekArray[week];
+  },
+
+  shareApp(){
+    console.log('share app');
+    
+  },
+  gotoWeatherKnowledge(){
+    console.log("gotoWeatherKnowledge")
+    wx.navigateTo({
+      url: '/pages/knowledge/knowledge',
+    })
+    wx.setStorage({
+      key: 'showKnowledgeDot',
+      data: 0,
+    })
+  },
+
+  reloadKnowledgeDotStatus(){
+    var that = this;
+    wx.getStorage({
+      key: 'showKnowledgeDot',
+      success: function(res) {
+        that.setData({
+          showKnowledgeDot:res.data
+        })
+      },
+    })
+  },
+
+  officialLoad(res){
+    console.log("officialLoad status:" + res.detail.status + ", msg:" + res.detail.errMsg)
+  },
+  officialerror(res){
+    console.log("officialerror status:" + res.detail.status + ", msg:" + res.detail.errMsg)
+  },
+  launchAppError(e) {
+    console.log("app launch:"+e.detail.errMsg)
+  },
+
+
+})
---
+++ pages/index/index.qml
@@ -0,0 +1,253 @@
+<view class="container {{isIPhoneX ? 'iphonex-padding' : ''}}" catchtap='menuHide'>
+  <heartbeat id='heartbeat' wx:if='{{showHeartbeat}}'></heartbeat>
+  <view class='bcg' wx:if='{{!bcgImg}}' style='background: {{bcgColor}}'></view>
+  <image class='bcg' wx:if='{{bcgImg}}' src='{{bcgImg}}' mode='aspectFill'></image>
+  
+  <!--搜索 start-->
+  <view id='search' class='search' wx:if='{{isShowSearch && !bcgImgAreaShow}}' style='background:rgba(255, 255, 255, 0);opacity:{{searchAlpha}}'>
+    <view class='wrapper'>
+      <image src='/img/search.png'></image>
+      <input placeholder-class='placeholderClass' confirm-type='search' placeholder='请输入城市名，快速查询天气信息' maxlength='20' bindconfirm='commitSearch' value='{{searchText}}' disabled='{{!enableSearch}}'></input>
+    </view>
+  </view>
+  <!--搜索 end-->
+
+  <!--tips-->
+  <view wx:if='{{!isShowLocationTips}}'> 
+    <view class='small_tips' catchtap='gotoWeatherKnowledge'>
+      <text>天气小知识</text>
+    </view>
+    <view class='dot' wx:if='{{showKnowledgeDot == 1}}'></view>
+  </view>
+
+  <!--引导-->
+   <view class='guid'  animation="{{animationHiddenGuidView}}">
+    <text>点击添加到我的桌面，下次使用更方便</text>
+   </view>
+  
+
+  <!--背景选择 start-->
+  <view class='chooseBcg' wx:if='{{bcgImgAreaShow}}'>
+    <view class='top'>
+      <view class='title'>更换背景</view>
+      <view class='bcgs'>
+        <view class='border {{bcgImgIndex === index ? "active" : ""}}' wx:for='{{bcgImgList}}' wx:key='{{index}}'>
+          <image src='{{item.src}}' catchtap='chooseBcg' data-index='{{index}}' data-src='{{item.src}}'></image>
+        </view>
+      </view>
+    </view>
+    <view class='close' catchtap='hideBcgImgArea'>
+      <image src='/img/up-arrow.png'></image>
+    </view>
+  </view>
+  <!--背景选择 end-->
+
+  <!--内容-->
+  <view id="mainView" class='content' wx:if='{{!bcgImgAreaShow}}' style='margin-top: {{setting.hiddenSearch ? 20 : 60}}px'>
+    <!--头像start-->
+    <view class='avatarInfo' catchtap='showBcgImgArea' wx:if='{{1 == 0}}'>
+      <open-data class='avatar' type='userAvatarUrl'></open-data>
+      <open-data class='name' type='userNickName'></open-data>
+      <image class='downArrow' src='/img/down.png'></image>
+    </view>
+    <!--头像end-->
+    <!--权限引导start-->
+    <view class='recommend' wx:if='{{isShowLocationTips}}'>
+        <view class='tips'>
+          需要使用定位权限，否则无法正常查看天气信息
+        </view>
+        <button class='btn' open-type="openSetting">
+          <image src='/img/location_s_w.png'></image>
+          <view>前往设置，开启定位权限</view>
+        </button>
+        
+    </view>
+    <!--权限引导end-->
+
+    <!--天气信息 start-->
+    <view class='info'>
+      <view class='city'>
+        <view class='name' bindtap='toCitychoose'>
+          <image wx:if='{{located}}' class='icon' src='/img/location_s_w.png'></image>
+          <view class='val'>{{cityDatas.basic.location || '定位中'}}</view>
+          <image class='down' src='/img/down.png'></image>
+        </view>
+        <text class='time' wx:if='{{cityDatas.updateTimeFormat}}'>{{cityDatas.updateTimeFormat}} 更新</text>
+      </view>
+      <view class='message'>{{message}}</view>
+      <view class='temp num' decode='true'>{{cityDatas.now.tmp || '-'}}
+        <text style='font-size:50rpx;position:relative;top:-20px;'>℃</text>
+      </view>
+      <view class='weather'>{{cityDatas.now.cond_txt || '--'}}</view>
+      <view class='pm'>
+        <text>PM2.5 {{air.pm25}}{{air.qlty}}</text>
+      </view>
+    </view>
+
+    <view class='todayAndTowmorr'>
+      <view class='detail' wx:for='{{cityDatas.daily_forecast}}' wx:key='{{index}}' wx:if='{{index<2}}'>
+        <view wx:if='{{index == 0}}'>
+          <text>今天</text>
+        </view>
+        <view wx:else>
+          <text>明天</text>
+        </view>
+        <view class='weather'>
+          <text>{{item.tmp_min}}~{{item.tmp_max}}℃</text>
+          <text style='margin-left: 10rpx'>{{item.cond_txt_d}}</text>
+          <image mode='widthFix' src='{{weatherIconUrl}}{{item.cond_code_d}}.png'></image>
+        </view>
+      </view>
+    </view>
+
+    <!--详细温度 start-->
+    <view class='weather_detail title' wx:for='{{cityDatas.daily_forecast}}' wx:key='{{index}}' wx:if='{{index<1}}'>
+    <text>实时详细预报</text>
+    </view>
+    <view class='details'>
+      <view class='detail' wx:for='{{detailsDic.key}}' wx:key='{{index}}'>
+        <view>{{detailsDic.val[item]}}</view>
+        <view>{{cityDatas.now[item]}}</view>
+      </view>
+    </view>
+    <!--详细温度 end-->
+
+    <!--分时温度 start-->
+    <view class='hourly' wx:if='{{hourlyDatas.length}}'>
+      <view class='title'>24 小时逐 3 小时预报</view>
+      <view class='hours'>
+        <swiper style='height:360rpx;' indicator-dots="{{false}}" autoplay="{{false}}" circular="{{false}}" duration="300" next-margin="50rpx">
+          <block wx:for="{{hourlyDatas}}" wx:key="{{index}}">
+            <swiper-item>
+              <view class='hour'>
+                <view class='detail'>
+                  <view>温度(℃)</view>
+                  <view>{{item.tmp}}</view>
+                </view>
+                <view class='detail'>
+                  <view>天气</view>
+                  <view class='weather'>
+                    <text>{{item.cond_txt}}</text>
+                    <image mode='widthFix' src='{{weatherIconUrl}}{{item.cond_code}}.png'></image>
+                  </view>
+                </view>
+                <view class='detail'>
+                  <view>相对湿度(%)</view>
+                  <view>{{item.hum}}</view>
+                </view>
+                <view class='detail'>
+                  <view>露点温度(℃)</view>
+                  <view>{{item.dew}}</view>
+                </view>
+                <view class='detail'>
+                  <view>降水概率</view>
+                  <view>{{item.pop}}</view>
+                </view>
+                <view class='detail'>
+                  <view>风向</view>
+                  <view>{{item.wind_dir}}</view>
+                </view>
+                <view class='detail'>
+                  <view>风向角度(deg)</view>
+                  <view>{{item.wind_deg}}</view>
+                </view>
+                <view class='detail'>
+                  <view>风力(级)</view>
+                  <view>{{item.wind_sc}}</view>
+                </view>
+                <view class='detail'>
+                  <view>风速(mk/h)</view>
+                  <view>{{item.wind_spd}}</view>
+                </view>
+                <view class='detail'>
+                  <view>气压(mb)</view>
+                  <view>{{item.pres}}</view>
+                </view>
+                <view class='detail'>
+                  <view>云量</view>
+                  <view>{{item.cloud}}</view>
+                </view>
+              </view>
+              <view class='time'>{{item.time}}</view>
+            </swiper-item>
+          </block>
+        </swiper>
+      </view>
+    </view>
+    <!--分时温度 end-->
+
+    <!--7预报start-->
+    <view class='guide' wx:if='{{cityDatas.daily_forecast}}'>
+      <view class='title'>7 天预报</view>
+      <view class='guides'>
+        <view class='item' wx:for='{{cityDatas.daily_forecast}}' wx:key='{{index}}'>
+          
+          <view class='date i' wx:if='{{index == 0}}'>今天</view>
+          <view class='date i' wx:else>{{item.date}}</view>
+          <view class='week i'>星期{{item.week}}</view>
+          <view class='temperature i'>{{item.tmp_min}}~{{item.tmp_max}}℃</view>
+          <view class='weather i'>
+            <text>{{item.cond_txt_d}}</text>
+            <image mode='widthFix' src='{{weatherIconUrl}}{{item.cond_code_d}}.png'></image>
+          </view>
+          <view class='wind i'>{{item.wind_dir}}{{item.wind_sc}}级</view>
+          <view class='weather i'>
+            <image mode='widthFix' src='{{richu}}'></image>
+            <text>{{item.sr}}</text>
+          </view>
+          <view class='weather i'>
+          <image mode='widthFix' src='{{riluo}}'></image>
+            <text>{{item.ss}}</text>
+          </view>
+        </view>
+      </view>
+    </view>
+    <!--7预报end-->
+
+    <!--推荐start-->
+    <view class='recommend'>
+        <view class='tips'>
+          提醒朋友注意天气变化，更贴心
+        </view>
+        <button class='btn' open-type="share">
+          <image src='/img/share.png'></image>
+          <view>分享给朋友</view>
+        </button>
+        
+    </view>
+    <!--推荐end-->
+
+    <!--生活指数-->
+    <view class='livingIndex'>
+      <view class='item' wx:for='{{cityDatas.lifestyle}}' wx:key='{{index}}'>
+        <image class='icon' src='/img/lifestyle_{{item.type}}.png'></image>
+        <view class='right'>
+          <view class='key'>{{lifestyles[item.type]}} {{item.brf}}</view>
+          <view class='value'>{{item.txt}}</view>
+        </view>
+      </view> 
+    </view>
+    <view class='footer'>到底了</view>
+
+    <!--公众号start-->
+    <official-account class='offical_font' bindload='officialLoad' binderror='officialerror'></official-account>
+    <!--公众号end-->
+  </view>
+  <!--天气信息 end-->
+  
+  
+  
+  <view wx:if='{{openSettingButtonShow}}' class='openSettingButton'>
+    <image src='/img/unlock.png'></image>
+    <button open-type='openSetting'></button>
+  </view>
+  <!-- 悬浮菜单 -->
+  <view class='menus' wx:if='{{!bcgImgAreaShow}}'>
+    <!-- <image src="/img/share_circle.png" animation="{{animationOne}}" class="menu"></image> -->
+    <!-- <button plain='true' open-type='share' animation="{{animationOne}}" class='menu share'></button> -->
+    <image src="/img/setting.png" animation="{{animationTwo}}" class="menu" catchtap="menuToSetting"></image>
+    <image src="/img/location.png" animation="{{animationThree}}" class="menu" catchtap="menuToCitychoose"></image>
+    <!-- <image src="/img/info.png" animation="{{animationFour}}" class="menu" catchtap="menuToAbout"></image> -->
+    <image src="/img/menu.png" animation="{{animationMain}}" class="menu main" catchtap="menuMain"></image>
+  </view>
+</view>
